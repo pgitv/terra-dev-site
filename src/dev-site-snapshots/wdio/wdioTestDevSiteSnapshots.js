234d94da8cfd8aa50ede3839e0cf33dc35156516
Ryan Manuel <rfmanuel@gmail.com>
2018-10-18 17:33:07 -0500

    Test

--- src/dev-site-snapshots/wdio/wdioTestDevSiteSnapshots.js
+++ src/dev-site-snapshots/wdio/wdioTestDevSiteSnapshots.js
@@ -1,39 +1,81 @@
+import SERVICE_DEFAULTS from 'terra-toolkit/config/wdio/services.default-config';
 import loadSiteConfig from '../../../scripts/generate-app-config/loadSiteConfig';
 import generatePagesConfig from '../../../scripts/generate-app-config/generatePagesConfig';
+
+const VIEWPORT_KEYS = Object.keys(SERVICE_DEFAULTS.terraViewports);
 
 const baseRouteOfExample = (siteConfig, pageKey) => {
   const baseRouteLink = siteConfig.navConfig.navigation.links.find(link => link.pageTypes.includes(pageKey));
   return baseRouteLink ? baseRouteLink.path : null;
 };
 
-const traversePathsFromTree = (currentPage, currentRoute, groupingDirectory = null) => {
-  describe(currentPage.name, () => {
-    if (currentPage.pages == null) {
-      beforeEach(() => {
-        browser.url(`/#/raw${currentRoute}${currentPage.path}`);
-      });
-      Terra.should.beAccessible();
-      Terra.should.matchScreenshot({ groupingDirectory });
-    } else {
-      currentPage.pages.forEach(mappedPage => traversePathsFromTree(mappedPage, `${currentRoute}${currentPage.path}`, currentPage.name));
-    }
+const mergeObjectOfArrays = (object1, object2) => {
+  const mergedObject = {};
+  VIEWPORT_KEYS.forEach((key) => {
+    mergedObject[key] = (object1[key] || []).concat(object2[key || []]);
+  });
+  return mergedObject;
+};
+
+const createViewportObjectFromPageTree = (currentPage, currentRoute, options = {}, groupingDirectory = null) => {
+  if (!currentPage.pages) {
+    // eslint-disable-next-line global-require, import/no-dynamic-require
+    const pageObject = require(currentPage.filePath);
+    const viewportObject = {};
+    pageObject.viewports.forEach((viewport) => {
+      viewportObject[viewport] = [{
+        name: currentPage.name,
+        groupingDirectory,
+        selector: pageObject.selector,
+        url: `/#/raw${currentRoute}${currentPage.path}`,
+      }];
+    });
+    return viewportObject;
+  }
+  let viewportObject = {};
+  currentPage.pages.forEach((subPage) => {
+    const subViewportObject = createViewportObjectFromPageTree(subPage, `${currentRoute}${currentPage.path}`, options, currentPage.name);
+    viewportObject = mergeObjectOfArrays(subViewportObject, subViewportObject);
+  });
+  return viewportObject;
+};
+
+const runTest = (test) => {
+  describe(test.name, () => {
+    global.before(() => {
+      global.browser.url(test.url);
+    });
+    global.Terra.should.beAccessible();
+    global.Terra.should.matchScreenshot({ groupingDirectory: test.groupingDirectory, selector: test.selector });
   });
 };
 
-const wdioTestDevSiteSnapshots = (options) => {
+const wdioTestDevSiteSnapshots = (options = {}) => {
   const siteConfig = loadSiteConfig();
   const pagesConfig = generatePagesConfig(siteConfig, true, false);
 
+  let viewportObject = {};
   Object.entries(pagesConfig).forEach((pagesEntry) => {
     const pageKey = pagesEntry[0];
     const baseRoute = baseRouteOfExample(siteConfig, pageKey);
     if (siteConfig.wdioPageTypes.includes(pageKey) && baseRoute) {
       pagesEntry[1].forEach((page) => {
         if (!options.package || page.path.includes(`/${options.package}/`)) {
-          traversePathsFromTree(page, baseRoute);
+          viewportObject = mergeObjectOfArrays(viewportObject, createViewportObjectFromPageTree(page, baseRoute, options));
         }
       });
     }
+  });
+
+  console.log('Viewport Object', JSON.stringify(viewportObject, null, 2));
+
+  Object.entries(viewportObject).forEach((viewportEntry) => {
+    describe(viewportEntry[0], () => {
+      global.before(() => {
+        global.browser.setViewportSize(global.Terra.viewports(viewportEntry[0])[0]);
+      });
+      viewportEntry[1].forEach(test => runTest(test));
+    });
   });
 };
 
--- src/terra-dev-site/test/TestWdioTestDevSiteSnapshots.test.jsx
+++ src/terra-dev-site/test/TestWdioTestDevSiteSnapshots.test.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 
+const viewports = ['tiny', 'small', 'medium', 'large', 'huge', 'enormous'];
+const selector = '#root';
+
+export { viewports, selector };
+
 export default () => (
   <div>Testing that wdio test dev site snapshots works.</div>
 );
